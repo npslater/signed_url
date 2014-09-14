b7d50f468a2286d2ec1df24076a1d2621931c3eb
Nate Slater <slatern@amazon.com>
2014-09-14 14:53:51 -0700

    Added --key option

--- lib/signed_url.rb
+++ lib/signed_url.rb
@@ -9,12 +9,17 @@ module SignedUrl
     desc 'sign', 'Creates an S3 object from the given file and generates a signed URL for doing a GET on the object'
     option :region, :required=>true
     option :bucket, :required=>true
-    option :file, :required=>true
+    option :file, :required=>false
+    option :key, :required=>false
     option :expiration_in_days, :required=>true, :type=>:numeric
     def sign
       s3 = AWS::S3.new(region:options[:region])
-      object_key = File.basename(options[:file])
-      s3.buckets[options[:bucket]].objects[object_key].write(file:options[:file])
+      if options[:file]
+        object_key = File.basename(options[:file])
+        s3.buckets[options[:bucket]].objects[object_key].write(file:options[:file])
+      else
+        object_key = options[:key]
+      end
       url = s3.buckets[options[:bucket]].objects[object_key].url_for(:read, expires:((60*60*24) * options[:expiration_in_days].to_i))
       puts url
       end
